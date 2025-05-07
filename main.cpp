179503675f006db1ff4aa47bc6a9e837f1c5d18d
Leon Lang <leon@langleon.com>
2025-05-07 23:14:39 +0200

    Changed Soft Shadows and improved random generated lightSources

--- main.cpp
+++ main.cpp
@@ -38,8 +38,8 @@ bool shadowIntersection(ObjectManager& objManager, const glm::vec3& lightPos, co
 	// shadowRay.origin += ray.direction * 0.001f; // add small value to prevent shadowAcne
 		// Prevents intersection between same object
 		// if (shadowObjFilename != currentObjFilename) {
-	for (int j = 0; j < triangles.size(); j++) {
-		float shadowDistance = Intersection::rayTriangleIntersection(shadowRay, triangles[j]);
+	for (int i = 0; i < triangles.size(); i++) {
+		float shadowDistance = Intersection::rayTriangleIntersection(shadowRay, triangles[i]);
 		if (shadowDistance != -INFINITY) {
 			return true;
 		}
@@ -48,55 +48,9 @@ bool shadowIntersection(ObjectManager& objManager, const glm::vec3& lightPos, co
 	return false;
 }
 
-// Soft Shadows
-// Generate multiple light Sources in near distance to each other
-// Tone Mapping
-// Concept: https://64.github.io/tonemapping/
-glm::vec3 softShadow(int& lightAmount,ObjectManager& objManager,const Triangle& triangle, const Ray& ray, const glm::vec3& lightPos, const float& distance) {
-	glm::vec3 objColor(1, 1, 0);
-	if (!triangle.textureName.empty()) {
-		glm::vec3 intersectionPoint = ray.origin + distance * ray.direction;
-		glm::vec2 interpolatedTexCoordinate = Graphics::getTextureCoordinate(Intersection::calculateBarycentricCoords(triangle, intersectionPoint), triangle.colorOneCoordinate, triangle.colorTwoCoordinate, triangle.colorThreeCoordinate);
-		// std::cout << "Interpolated X:" << interpolatedTexCoordinate.x << "Xone:" << triangle->colorOneCoordinate.x << "Xtwo:" << triangle->colorTwoCoordinate.x << "XThree:" << triangle->colorThreeCoordinate.x << std::endl;
-		unsigned char* texData = objManager.textureData[triangle.textureName];
-		glm::ivec2 texDim = objManager.textureDimensions[triangle.textureName];
-
-		size_t texIndex = (static_cast<int>(interpolatedTexCoordinate.y) *texDim.x + static_cast<int>(interpolatedTexCoordinate.x)) * 3;
-		objColor.x = texData[texIndex + 0] / 255.0f;
-		objColor.y = texData[texIndex + 1] / 255.0f;
-		objColor.z = texData[texIndex + 2] / 255.0f;
-	}
-	glm::vec3 color(0.0f,0.0f,0.0f);
-	glm::vec3 lightPosChanged = lightPos;
-	// Generate the lightPos based on the last lightPos and change always one coordinate
-	/*
-	for (int i = 0; i < lightAmount; i++) {
-		bool isShadow = shadowIntersection(objManager, lightPosChanged, distance, ray);
-		glm::vec3 colorPhong = phongIllumination(triangle, ray, lightPosChanged, objColor, distance);
-		// glm::vec3 colorPhong (240.f,1.f,1.f);
-		if (isShadow) { colorPhong /= 5; };
-		color += colorPhong;
-		// Before it was 2
-		switch (i % 3) {
-		case 0:
-			lightPosChanged.x += 3.0f;
-			break;
-		case 1:
-			lightPosChanged.y += 3.0f; 
-			break;
-		case 2:
-			lightPosChanged.z += 3.0f;
-			break;
-		}
-	}
-	*/
-	// Disable Shadows for testing purposes
-	color = Graphics::phongIllumination(objManager,triangle, ray, lightPosChanged, distance);
-
-
-	// Reinhardt Tone Mapping 
-	// color = color / (color + 1.0f);
-	color = color / (color + 0.5f);
+void reinhardtToneMapping(glm::vec3& color) {
+	color = color / (color + 1.0f);
+	// color = color / (color + 0.5f);
 	// color = color / (color + 0.1f);
 	// color = color / (color + 4.0f);
 
@@ -104,13 +58,31 @@ glm::vec3 softShadow(int& lightAmount,ObjectManager& objManager,const Triangle& 
 	glm::vec3 gamma(1.1f, 1.1f, 1.1f);
 	// glm::vec3 gamma (2.2f, 2.2f, 2.2f);
 	color = glm::pow(color, gamma);
-
-	return color;
 }
+std::vector<glm::vec3> generateRandomCoordinates(int amount, float range) {
+	std::vector<glm::vec3> coordinates;
+	glm::vec3 coordinate(0, 0, 0);
+	for (int i = 0; i < amount; i++) {
+		switch (i % 3) {
+		case 0:
+			coordinate.x += range;
+			break;
+		case 1:
+			coordinate.y += range; 
+			break;
+		case 2:
+			coordinate.z += range;
+			break;
+		}
+		coordinates.push_back(coordinate);
+	}
+	return coordinates;
+}
+
 
 // Ray Intersection with Boxes and Triangles
 // Combines the Methods for Slab Test, Bounding Box Volume Hierarchy and Triangle Intersection
-std::pair<glm::vec2, glm::vec3> rayIntersection(const Ray& ray, ObjectManager& objManager, const int& pointX,const int& pointY, const glm::vec3& lightPos) {
+glm::vec3 rayIntersection(const Ray& ray, ObjectManager& objManager, const glm::vec3& lightPos, std::vector<glm::vec3>& randomCoordinates) {
 
 	glm::vec3 colorPoint(0, 0, 0);
 	float distanceComparison = INFINITY;
@@ -121,8 +93,14 @@ std::pair<glm::vec2, glm::vec3> rayIntersection(const Ray& ray, ObjectManager& o
 		if (fDistance != -INFINITY && fDistance < distanceComparison) {
 			distanceComparison = fDistance;
 
-			int lightAmount = 1;
 			glm::vec3 color = Graphics::phongIllumination(objManager,objManager.triangles.at(k), ray, lightPos, fDistance);
+
+			int shadowAmount = 0;
+			for (const glm::vec3& point : randomCoordinates) {
+				glm::vec3 lightPosChanged = lightPos + point;
+				if (shadowIntersection(objManager, lightPosChanged, fDistance, ray)) { shadowAmount++; };
+			}
+			std::cout << "Shadow Amount: " << shadowAmount << std::endl;
 			// glm::vec3 color = softShadow(lightAmount,objManager, objManager.triangles.at(k), ray,lightPos,fDistance);
 			// Convert 0...1 color values to 1...255 color Values
 			colorPoint.x = int((color.x * 255));
@@ -130,29 +108,29 @@ std::pair<glm::vec2, glm::vec3> rayIntersection(const Ray& ray, ObjectManager& o
 			colorPoint.z = int((color.z * 255));
 		}
 	} 
-	glm::vec2 imagePoint(pointX, pointY);
-	return { imagePoint, colorPoint };
+	return colorPoint;
 }
+
+
 
 
 // Sending out Rays
 // Concept: https://cg.informatik.uni-freiburg.de/course\_notes/graphics\_01\_raycasting.pdf
 // Sends out Rays and returns the corresponding color for each pixel
-
 ImageData sendRaysAndIntersectPointsColors(const glm::vec2& imageSize, const glm::vec4& lightPos, ObjectManager& objManager) {
 	Ray ray(glm::vec3(0.0f, 0.0f, 400.0f));
 	glm::vec2 rayXY = glm::vec2(ray.direction.x, ray.direction.y);
 	ImageData imageData;
-
+	std::vector<glm::vec3> randomCoordinates = generateRandomCoordinates(5,3.0f);
 	for (int i = 0; i < imageSize.x; ++i) {
 		for (int j = 0; j < imageSize.y; ++j) {
 			ray.direction.x = i + rayXY.x - imageSize.x / 2;
 			ray.direction.y = j + rayXY.y - imageSize.y / 2;
 
-			std::pair<glm::vec2, glm::vec3> points = rayIntersection(ray, objManager, i, j, lightPos);
-			if (points.second != glm::vec3(0, 0, 0)) {
-				imageData.imagePoints.push_back(points.first);
-				imageData.imageColors.push_back(points.second);
+			glm::vec3 colorPoint = rayIntersection(ray, objManager, lightPos, randomCoordinates);
+			if (colorPoint != glm::vec3(0, 0, 0)) {
+				imageData.imagePoints.push_back(glm::vec2(i, j));
+				imageData.imageColors.push_back(colorPoint);
 			}
 		}	
 	}
